200ea38b77ec3f38b70c2cf3981c3fb0fbf0b2b2
Daniel Kuroski <danielkuroski@Daniels-Mac-mini.local>
2016-06-22 23:48:37 -0300

    Instalando mirage para testes e configurando url API

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    apiNamespace: 'api/v1',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -25,6 +26,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.serverURL = 'http://red-greenapi.herokuapp.com'
   }
 
   if (environment === 'test') {
@@ -43,5 +45,7 @@ module.exports = function(environment) {
 
   }
 
+  ENV.apiBaseURL = ENV.serverURL + '/' + ENV.apiNamespace;
+  
   return ENV;
 };
